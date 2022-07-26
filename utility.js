4cd383a149110dfd22c94a8ce66f2bd40cb4e829
PhithakB <phithakb2000s@gmail.com>
2022-07-26 17:14:04 +0700

    codelab5

--- utility.js
+++ utility.js
@@ -1,8 +1,8 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API=""; // put url
-const accessToken=""; // put access token
+const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODMwMDAzLCJleHAiOjE2NTg4MzM2MDMsIm5iZiI6MTY1ODgzMDAwMywianRpIjoiVHRpSnZzNHBrS09MQkN2byIsInN1YiI6NiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.Q3Pqw-g8R3exp2yUWjb8yqYi2WCNjoxu5nLq1zEhRbk"; // put access token
 const student = {
     student_id: 63364376,
   name: '', // replace with your full name.
@@ -13,34 +13,29 @@ const student = {
 
 
 exports.findStudentbyId = function (student_id, cb) {
-  //-- call external api 
-          request({
-            method: "POST",
-            uri: EXTERNAL_API,
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-            formData: {
-                message: `HTTP Request :${data.student_id} `,
-                student_id: student_id
-            }
-        }).then((response) => {
-            console.log('Sent');
-            console.log({
-                "request": student_id,
-                "response": response
-            });
-
-           cb(response);
-
-        }).catch((err) => {
-            console.log('Error:', err.message);
-            console.log({
-                "Error": err.message
-            });
-        });
-}
-
+    //-- call external api 
+            request({
+              method: "GET",
+              uri: EXTERNAL_API+student_id,
+              headers: {
+                  Authorization: `Bearer ${accessToken}`
+              }
+          }).then((response) => {
+              console.log('Sent');
+              console.log({
+                  "request": student_id,
+                  "response": response
+              });
+  
+             cb(response);
+  
+          }).catch((err) => {
+              console.log('Error:', err.message);
+              console.log({
+                  "Error": err.message
+              });
+          });
+  }
 exports.fakeStudentbyInfo = function (student_id, cb) {
 
   cb(student);
