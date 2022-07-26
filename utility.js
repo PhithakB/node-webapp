14809fd9362c754e7a50b01364f9875e4e3c9100
PhithakB <phithakb2000s@gmail.com>
2022-07-27 00:21:30 +0700

    add Login page

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODMwMDAzLCJleHAiOjE2NTg4MzM2MDMsIm5iZiI6MTY1ODgzMDAwMywianRpIjoiVHRpSnZzNHBrS09MQkN2byIsInN1YiI6NiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.Q3Pqw-g8R3exp2yUWjb8yqYi2WCNjoxu5nLq1zEhRbk"; // put access token
+const accessToken="" // put access token
 const student = {
     student_id: 63364376,
   name: '', // replace with your full name.
