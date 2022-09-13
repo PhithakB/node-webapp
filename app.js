18cbecc0ac3f92399b6c778eb4f95257222844be
PhithakB <phithakb2000s@gmail.com>
2022-09-13 20:09:34 +0700

    format Doc

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 //app.js
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-var util = require('./utility');
-
+var util = require("./utility");
 
 var app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 /** specify the directory from where to serve static assets such as JavaScript, CSS, images **/
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-app.use('/jquery-ui', express.static(__dirname + '/node_modules/jquery-ui/dist/'));
+app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist/"));
+app.use(
+  "/jquery-ui",
+  express.static(__dirname + "/node_modules/jquery-ui/dist/")
+);
 
 /** remove fix route and use path solution **/
 
@@ -28,44 +30,41 @@ app.get('/', function(req, res) {
 });
 **/
 
-app.get('/welcome', (req, res) =>
+app.get("/welcome", (req, res) =>
   res.send({
     error: false,
-    message: 'Welcome NodeJS, Express',
-  }),
+    message: "Welcome NodeJS, Express",
+  })
 );
 
-app.post('/infomation', function(req, res) {
-   
-   var result ={ student_id: req.body.student_id,
-     firstname: req.body.firstname,
-     lastname: req.body.lastname,
-     gender: req.body.gender
-   }
+app.post("/infomation", function (req, res) {
+  var result = {
+    student_id: req.body.student_id,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    gender: req.body.gender,
+  };
 
   res.json(result);
-
 });
 
-app.get('/release', (req, res) =>{
-   res.status(400).send("error aria?");
+app.get("/release", (req, res) => {
+  res.status(400).send("error aria?");
 });
 
-app.get('/ok', (req, res) =>{
-   res.status(200).json({ "status": true, "result": ' successful!' });
+app.get("/ok", (req, res) => {
+  res.status(200).json({ status: true, result: " successful!" });
 });
 
-
-app.get('/student/:student_id', function(req, res) {
+app.get("/student/:student_id", function (req, res) {
   util.findStudentbyId(req.params.student_id, function (result) {
-      //res.json(result);
-      res.send(result);
+    //res.json(result);
+    res.send(result);
   });
 });
-
 
 var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-    console.log('Starting node.js on port ' + port);
+  console.log("Starting node.js on port " + port);
 });
--- utility.js
+++ utility.js
@@ -1,41 +1,40 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
+const EXTERNAL_API = "https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
 const student = {
-    student_id: 63364376,
-  name: '', // replace with your full name.
+  student_id: 63364376,
+  name: "", // replace with your full name.
   age: 21, // put your age.
-  gender: '', // replace with your gender
-  department: 'CPE'
+  gender: "", // replace with your gender
+  department: "CPE",
 };
 
-
 exports.findStudentbyId = function (student_id, cb) {
-    //-- call external api 
-            request({
-              method: "GET",
-              uri: EXTERNAL_API+student_id,
-              headers: {
-                  Authorization: `Bearer ${accessToken}`
-              }
-          }).then((response) => {
-              console.log('Sent');
-              console.log({
-                  "request": student_id,
-                  "response": response
-              });
-  
-             cb(response);
-  
-          }).catch((err) => {
-              console.log('Error:', err.message);
-              console.log({
-                  "Error": err.message
-              });
-          });
-  }
-exports.fakeStudentbyInfo = function (student_id, cb) {
+  //-- call external api
+  request({
+    method: "GET",
+    uri: EXTERNAL_API + student_id,
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((response) => {
+      console.log("Sent");
+      console.log({
+        request: student_id,
+        response: response,
+      });
 
+      cb(response);
+    })
+    .catch((err) => {
+      console.log("Error:", err.message);
+      console.log({
+        Error: err.message,
+      });
+    });
+};
+exports.fakeStudentbyInfo = function (student_id, cb) {
   cb(student);
-}
+};
